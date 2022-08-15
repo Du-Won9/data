8d3d8cabf41e53ec1a52fba4c99a8776cfd76761
Du-Won9 <107368010+Du-Won9@users.noreply.github.com>
2022-08-15 09:08:04 +0630

    Add files via upload

---
+++ pageDATA.js
@@ -0,0 +1,17 @@
+let PGSdata = [
+    {
+    id:"AnimationWorldIDAT14820223008",
+    embed:"/storage/emulated/0/Data/AnimationWorld.jpg",
+    name:"💛Animation World💛",
+    home:"www.go.com",
+    ID: function() {
+        IDSET("AnimationWorldIDAT14820223008");
+        this.style.backgroundColor = b;
+        this.style.color = c;
+        this.style.fontFamily = f;
+    },
+    letfetch: function() {
+        open("www.go.com");
+    }
+}
+];
---
+++ postDATA.js
@@ -0,0 +1,52 @@
+var postDATA=[
+    {
+     id:"Hello",
+     pid:"HelloFAV",
+     adm:"💛Marz Beta💛", 
+     date:"14/8/2022",
+     type:"App Creator",
+     admLOGO:"/storage/emulated/0/Data/IMG_20220728_102626.jpg",
+     photos:["/storage/emulated/0/Data/IMG_20220728_102626.jpg"],
+     desc:"http://localhost:26543/storage/emulated/0/Project Folder/Memory/welcom.txt",
+     home:"myHome.com",
+     fav:function(){
+         setFav("HelloFAV")
+     },
+     rml:function(){
+         RMFav("HelloFAV")
+     }
+     },
+    {
+     id:"AnimationWorldIDAT14820223008",
+     pid:"AnimationWorldPID1",
+     adm:"💛Animation World💛", 
+     date:"14/8/2022",
+     type:"Personal Blog",
+     admLOGO:"/storage/emulated/0/Data/AnimationWorld.jpg",
+     photos:["/storage/emulated/0/Data/AnimationWorld.jpg"],
+     desc:"http://localhost:26543/storage/emulated/0/Project Folder/Memory/ID1.txt",
+     home:"file:///android_asset/layout.html",
+     fav:function(){
+         setFav("AnimationWorldPID1")
+     },
+     rml:function(){
+         RMFav("AnimationWorldPID1")
+     }
+     },{
+     id:"AnimationWorldIDAT14820223008",
+     pid:"AnimationWorldPID2",
+     adm:"💛Animation World💛", 
+     date:"14/8/2022",
+     home:"go.com",
+     type:"Personal Blog",
+     admLOGO:"/storage/emulated/0/Data/a37762a898c7cf8c428b68ce749ae7bf.jpg",
+     photos:["http://localhost:26543/storage/emulated/0/Data/d7b7e8dd22b2c7646398fc43704a13c7.jpg","http://localhost:26543/storage/emulated/0/Data/Admin.jpg","http://localhost:26543/storage/emulated/0/DCIM/Facebook/FB_IMG_1657661338516.jpg","http://localhost:26543/storage/emulated/0/Data/received_3171371856445814.jpeg","http://localhost:26543/storage/emulated/0/DCIM/Screenshots/IMG_20220621_195146.jpg"],
+     desc:"http://localhost:26543/storage/emulated/0/Project Folder/Memory/efg.txt",
+     fav:function(){
+         setFav("AnimationWorldPID2")
+     },
+     rml:function(){
+         RMFav("AnimationWorldPID2")
+     }
+     }
+    ];
